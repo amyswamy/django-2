b269e92a3e20476a91484558f4cbdc5dc0af3941
amyswamy <50063000@belfastmet.ac.uk>
2024-11-04 10:18:28 +0000

    ok

---
+++ core/events/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+from django.db import models
+from django.contrib.auth.models import User
+class Recipe(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    recipe_name = events.CharField(max_length=300)
+    recipe_description = models.TextField()
+    recipe_image = models.ImageField(upload_to="recipe")
+    recipe_view_count = models.PositiveIntegerField(default=1)
+    
